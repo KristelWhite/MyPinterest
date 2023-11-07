4c858b837b4c3389036de853139156bfeb325707
KristelWhite <KristelWhite@users.noreply.github.com>
2023-11-07 19:33:35 +0600

    Initial Commit

---
+++ MyPinterest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPinterest
+//
+//  Created by Кристина Пастухова on 07.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
